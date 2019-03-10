7d06aa823ecad37c5b2da225f3e30f2d71e64080
mightykukulkan <uma.s.shankar@gmail.com>
2019-03-10 16:35:26 +0530

    file upload completed

--- app/routes/features.js
+++ app/routes/features.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+
+    model(){
+        return {"data":["passedemissions","mpg","cylinders","displacement","horsepower",	
+        "weight",	"acceleration",	"modelyear",	"carname"]}
+    }
 });
--- app/routes/home.js
+++ app/routes/home.js
@@ -1,4 +1,26 @@
 import Route from '@ember/routing/route';
+import { get, set } from '@ember/object';
+import { task } from 'ember-concurrency';
+var mycontroller;
 
 export default Route.extend({
+
+  model(){
+    mycontroller = this.controllerFor('home')
+  },
+
+  uploadPhoto: task(function * (file) {
+      let response = yield file.upload('http://localhost:3000/fileupload');   
+      if(response.status === 200){
+        alert("upload successfull")
+        mycontroller.set('next', true)      
+      }
+  }).maxConcurrency(1).enqueue(),
+
+  actions: {
+    uploadImage(file) {
+      console.log("uploadimage is being called")
+      get(this, 'uploadPhoto').perform(file);
+    }
+  }
 });
